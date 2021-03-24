31f9fe2cbc46ffdf67e6bd1bbeadd9a3ae50fab3
gwise <gwise@GWISE>
2021-03-24 17:16:09 +0900

    2021.03.24

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -48,7 +48,15 @@ public class ApplicationContextExtendsFindTest {
             System.out.println("key = "  + key + " values=" + beansOfType.get(key));
         }
     }
-    
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+    void findAllBeanByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for(String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value=" + beansOfType.get(key));
+        }
+    }
     
     @Configuration
     static class TestConfig {
